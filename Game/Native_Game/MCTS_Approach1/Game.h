e131cb666833476e356ebb40bb20e4fb3706f647
jh97kim <jh97kim@student.cs.uwaterloo.ca>
2018-07-28 11:28:35 -0400

    Compiled Optimized code in Linux Environment

--- Game/Native_Game/MCTS_Approach1/Game.h
+++ Game/Native_Game/MCTS_Approach1/Game.h
@@ -6,11 +6,11 @@
 #define ULTIMATE_TIC_TAC_TOE_ZERO_GAME_H
 
 #include "memory"
-#include "board.h"
+#include "Board.h"
 
 class Node;
 
-std::unique_ptr<Node>& getMove(Node* node, double time = 2.5);
+std::unique_ptr<Node>& getMove(Node* node, double time = 3);
 
 std::unique_ptr<Node>& getChildVisitedMost(Node* node);
 std::unique_ptr<Node>& getChildHighestUCT(Node* node);
---
+++ Game/Native_Game/MCTS_Approach1/main.d
@@ -0,0 +1,1 @@
+main.o: main.cc Game.h Board.h Node.h
